e373684ea89c2caf5c07e80bfccb1347c958419c
Jack Henry <jackson.lewis.henry@gmail.com>
2021-04-09 07:27:27 -0400

    done with mvp

---
+++ src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
@@ -0,0 +1,61 @@
+package com.lambdaschool.piggybank.controllers;
+
+import com.lambdaschool.piggybank.models.Coin;
+import com.lambdaschool.piggybank.repositories.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CoinController {
+    @Autowired
+    private CoinRepository coinRepository;
+
+    //http://localhost:2019/total
+    //Create an endpoint http://localhost:2019/total that prints to console the contents of the Piggy Bank as follows and returns an HTTP Status of OK:
+    @GetMapping(value = "/total", produces = "application/json")
+    public ResponseEntity<?> printTotal() {
+        List<Coin> coinList = new ArrayList<>();
+        coinRepository.findAll().iterator().forEachRemaining(c -> coinList.add(c));
+
+        double total = 0.00;
+        for (Coin c : coinList) {
+            total += c.getQuantity() * c.getValue();
+        }
+        System.out.println(
+                coinList.get(0).getQuantity() + " " + coinList.get(0).getName() + "\n"
+                + coinList.get(1).getQuantity() + " " + coinList.get(1).getName() + "\n"
+                + coinList.get(2).getQuantity() + " " + coinList.get(2).getNameplural() + "\n"
+                + coinList.get(3).getQuantity() + " " + coinList.get(3).getNameplural() + "\n"
+                + coinList.get(4).getQuantity() + " " + coinList.get(4).getNameplural() + "\n"
+                + coinList.get(5).getQuantity() + " " + coinList.get(5).getName() + "\n"
+                + coinList.get(6).getQuantity() + " " + coinList.get(6).getNameplural() + "\n"
+                + "The piggy bank holds " + total
+        );
+
+        //MVP in console
+        //    1 Quarter
+        //    1 Dime
+        //    5 Dollars
+        //    3 Nickels
+        //    7 Dimes
+        //    1 Dollar
+        //    10 Pennies
+        //    The piggy bank holds 7.3
+
+//        System.out.println(con);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+
+    //Note: 7.30 = ( 1 * 0.25) + ( 1 * 0.10) + ( 5 * 1.00) + ( 3 * 0.05) + ( 7 * 0.10) + ( 1 * 1.00) + (10 * 0.10)
+    //
+    //Note: that when you have more than 1 coin, the plural of the coin's name is printed.
+}
